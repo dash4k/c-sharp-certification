7ee1a14a14c4d9b99f462f9b2f66b31ec5c0c4b2
dash4k <danishwara81@gmail.com>
2025-03-22 14:51:44 +0800

    update progress

--- logic-applications/Program.cs
+++ logic-applications/Program.cs
@@ -1,10 +1,34 @@
-﻿bool flag = true;
-int value = 0;
+﻿// Code project 1
 
-if (flag) 
-{
-    Console.WriteLine($"Inside the code block: {value}");
-}
+// string? readResult;
+// int userValue = 0;
+// bool validEntry = false;
+// Console.WriteLine("Enter an integer value between 5 and 10: ");
 
-value = 10;
-Console.WriteLine($"Outside the code block: {value}");
+// while (!validEntry)
+// {
+//     readResult = Console.ReadLine();
+//     if (readResult != null)
+//     {
+//         if (int.TryParse(readResult, out userValue))
+//         {
+//             if (userValue > 4 && userValue < 11)
+//             {
+//                 validEntry = true;
+//             }
+//             else
+//             {
+//                 Console.WriteLine($"You entered {userValue}. Please enter a number between 5 and 10.");
+//             }
+//         }
+//         else
+//         {
+//             Console.WriteLine("Sorry, you entered an invalid number, please try again.");
+//         }
+//     }
+// }
+// Console.WriteLine($"Your input value ({userValue}) has been accepted.");
+
+
+
+// Code project 2
